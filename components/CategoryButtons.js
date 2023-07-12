adfb85740108acb95e0df2d986fd3f1f6e643d40
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-07-12 09:14:56 -0500

    Botón de Meats

--- components/CategoryButtons.js
+++ components/CategoryButtons.js
@@ -6,25 +6,29 @@ const CategoryButtons = () => {
     const navigation = useNavigation();
 
     const handleAddPress = () => {
-    navigation.navigate('NewSaucer');
-};
+        navigation.navigate('NewSaucer');
+    };
+
+    const handleMeatsPress = () => {
+        navigation.navigate('NewMeats');
+    };
 
     return (
-    <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>All</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Meats</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Others</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={handleAddPress}>
-            <Text style={styles.buttonText}>Add Saucer</Text>
-        </TouchableOpacity>
-    </View>
-);
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>All</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Others</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleMeatsPress}>
+                <Text style={styles.buttonText}>Add Meats</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleAddPress}>
+                <Text style={styles.buttonText}>Add Saucer</Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -36,19 +40,19 @@ const styles = StyleSheet.create({
         paddingHorizontal: 270,
         backgroundColor: '#D9D9D9',
         marginTop: 10,
-},
+    },
     button: {
         paddingHorizontal: 15,
         paddingVertical: 8,
         borderRadius: 5,
         backgroundColor: '#8EC8D0',
         marginHorizontal: 40,
-},
+    },
     buttonText: {
         color: 'black',
         fontSize: 16,
         fontWeight: 'bold',
-},
+    },
 });
 
 export default CategoryButtons;
