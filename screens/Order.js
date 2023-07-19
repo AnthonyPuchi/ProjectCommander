84f40e0eb2f4d8fdcedfb57fc1add29c3d1f373f
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-07-18 23:15:26 -0500

    Update Order

--- screens/Order.js
+++ screens/Order.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import Petition from '../components/Petition';
+import CymbalReview from '../components/CymbalReview';
 import HeaderStart from '../components/HeaderStart';
-import CategoryButtons from '../components/CategoryButtons';
 
 const Order = () => {
   return (
     <View style={styles.container}>
-        <HeaderStart />
-        <View style={styles.categoryContainer}>
-        <CategoryButtons />
+      <HeaderStart />
+      <View style={styles.contentContainer}>
+        <View style={styles.petition}>
+          <Petition />
         </View>
-        
+        <View style={styles.review}>
+          <CymbalReview />
+        </View>
+      </View>
     </View>
   );
 };
@@ -18,11 +23,22 @@ const Order = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D6BD39',
   },
-  categoryContainer: {
-    
-
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  petition: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FED151',
+  },
+  review: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#71B280',
   },
 });
 
