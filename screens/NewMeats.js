956d457dedb15fb6b2dc0a5128f889c4b1d40fa2
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-08-04 08:19:50 -0500

    Update

--- screens/NewMeats.js
+++ screens/NewMeats.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Button, StyleSheet, Alert } from 'react-native';
+import { View, Button, StyleSheet, Alert, Picker } from 'react-native';
 import HeaderStart from '../components/HeaderStart';
 import Input from '../components/Input';
 import { createAPI, proteins } from '../service/ServiceSaucer';
@@ -7,22 +7,31 @@ import { createAPI, proteins } from '../service/ServiceSaucer';
 const NewMeats = () => {
   const [name, setName] = useState('');
   const [quantity, setQuantity] = useState('');
+  const [stockMin, setStockMin] = useState('');
+  const [stockMax, setStockMax] = useState('');
+
 
   const handleCancel = () => {
     setName('');
     setQuantity('');
+    setStockMin('');
+    setStockMax('');
   };
 
   const handleSave = async () => {
     const data = {
       name,
       quantity,
+      stockMin,
+      stockMax,
     };
 
     try {
       const response = await createAPI(proteins, data);
       setName('');
       setQuantity('');
+      setStockMin('');
+      setStockMax('');
       Alert.alert('Success', 'Saucer saved successfully');
     } catch (error) {
       console.log('Error al guardar los platillos:', error);
@@ -34,21 +43,35 @@ const NewMeats = () => {
       <HeaderStart />
       <View style={styles.newSaucerContainer}>
         <Input
-          label="Name"
-          placeholder="Name"
+          label="Nombre de la carne"
+          placeholder="Nombre de la carne"
           value={name}
           onChangeText={(text) => setName(text)}
         />
         <Input
-          label="Quantity"
-          placeholder="Quantity"
+          label="Cantidad de Porciones"
+          placeholder="Cantidad de Porciones"
           value={quantity}
           onChangeText={(text) => setQuantity(text)}
           keyboardType="numeric"
         />
+        <Input
+          label="Stock Mínimo"
+          placeholder="Stock Mínimo"
+          value={stockMin}
+          onChangeText={(text) => setStockMin(text)}
+          keyboardType="numeric"
+        />
+        <Input
+          label="Stock Máximo"
+          placeholder="Stock Máximo"
+          value={stockMax}
+          onChangeText={(text) => setStockMax(text)}
+          keyboardType="numeric"
+        />
         <View style={styles.buttonContainer}>
-          <Button title="Cancel" onPress={handleCancel} color="red" />
-          <Button title="Save" onPress={handleSave} color="green" />
+          <Button title="Cancelar" onPress={handleCancel} color="red" />
+          <Button title="Guardar" onPress={handleSave} color="green" />
         </View>
       </View>
     </View>
@@ -58,7 +81,7 @@ const NewMeats = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D6BD39',
+    backgroundColor: '#4EC28E',
   },
   newSaucerContainer: {
     flex: 1,
@@ -76,3 +99,4 @@ const styles = StyleSheet.create({
 });
 
 export default NewMeats;
+
