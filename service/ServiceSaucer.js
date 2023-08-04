cce7ea346e9c580ac2e6f1f14b15c22fb5317ea1
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-08-04 08:15:26 -0500

    Nuevas API

--- service/ServiceSaucer.js
+++ service/ServiceSaucer.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
 export const saucer = 'http://localhost:8081/saucer';
+export const proteins = 'http://localhost:8081/proteins';
+export const saucerProteinsSaucer = 'http://localhost:8081/saucer/proteins-saucer';
+export const saucerProteins = 'http://localhost:8081/saucerProteins';
+export const petition = 'http://localhost:8081/petition';
+export const saucerProteinsPortions = 'http://localhost:8081/saucerProteins/portions';
+
 
 export const getAPI = async (url) => {
     const response = await axios.get(url);
